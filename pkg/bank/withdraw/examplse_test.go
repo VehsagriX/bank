db7d751dadc044ff3f92b9349a24a3b47bd0169a
Vehsagri Xasan <khasan.irgashev.97@gmail.com>
2026-02-10 19:49:57 +0500

    create with-draw pkg

---
+++ pkg/bank/withdraw/examplse_test.go
@@ -0,0 +1,15 @@
+package withdraw
+
+import (
+	"fmt"
+
+	"github.com/VehsagriX/bank/pkg/bank/types"
+)
+
+func ExampleWithDraw() {
+	card := types.Card{Balance: 20_000_00, Active: true}
+	WithDraw(&card, 10_000_00)
+	fmt.Println(card.Balance)
+	//Output: 1000000
+
+}
