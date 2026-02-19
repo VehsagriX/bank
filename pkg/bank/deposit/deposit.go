7bc2665471beaf68b662bbd31394140b98062ecf
Vehsagri Xasan <khasan.irgashev.97@gmail.com>
2026-02-19 19:34:33 +0500

    create constant in deposit

--- pkg/bank/deposit/deposit.go
+++ pkg/bank/deposit/deposit.go
@@ -2,8 +2,10 @@ package deposit
 
 import "github.com/VehsagriX/bank/pkg/bank/types"
 
+const depositLimit = 50_000
+
 func Deposit(card *types.Card, amount types.Money) {
-	if card.Active && amount <= 50_000 {
+	if card.Active && amount <= depositLimit {
 		card.Balance += amount
 	}
 	return
